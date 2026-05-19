cd9b63d4c9e8fbc02dccd7ea856dd9f0b51e05e1
foriver4725 <hrt67@icloud.com>
2026-05-19 16:31:56 +0900

    Update to Unity6.3, deleted redundant methods.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,99 +1,191 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
-using Ex;
 using TMPro;
-using Math = Ex.Math;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Main : MonoBehaviour
 {
-    public GameObject[] Prfbs;
-    public TMP_InputField PlayerInput;
+    private enum Stone : byte
+    {
+        None = 0,
+        Black = 1,
+        White = 2,
+    }
+
+    [SerializeField] private GameObject emptyStonePrefab;
+    [SerializeField] private GameObject blackStonePrefab;
+    [SerializeField] private GameObject whiteStonePrefab;
+
+    [SerializeField] private TMP_InputField inputField;
+
+    private readonly Dictionary<Stone, GameObject> _prefabs = new()
+    {
+        { Stone.None, null },
+        { Stone.Black, null },
+        { Stone.White, null },
+    };
+
+    private static readonly Dictionary<Vector3Int, Stone> InitialStones = new()
+    {
+        { new Vector3Int(3, 3, 3), Stone.Black },
+        { new Vector3Int(4, 3, 3), Stone.White },
+        { new Vector3Int(3, 4, 3), Stone.White },
+        { new Vector3Int(4, 4, 3), Stone.Black },
+        { new Vector3Int(3, 3, 4), Stone.White },
+        { new Vector3Int(4, 3, 4), Stone.Black },
+        { new Vector3Int(3, 4, 4), Stone.Black },
+        { new Vector3Int(4, 4, 4), Stone.White },
+    };
 
     // Must be even.
-    static readonly int fieldSize = 8;
+    private const int FieldSize = 8;
 
-    // 0:nothing, 1:black, 2:white. [0, 0, 0] ~ [7, 7, 7].
-    GameObject[,,] fieldObj = new GameObject[fieldSize, fieldSize, fieldSize];
-    int[,,] field = new int[fieldSize, fieldSize, fieldSize];
-    bool isBlackTurn = true;
+    private readonly GameObject[] _fieldObj = new GameObject[FieldSize * FieldSize * FieldSize];
+    private readonly Stone[] _field = new Stone[FieldSize * FieldSize * FieldSize];
+    private bool _isBlackTurn = true;
 
-    void Start()
+    private void Start()
     {
-        // Set up the field.
-        foreach (Vector3Int e in Collection.Enumerate(fieldSize, fieldSize, fieldSize))
-        {
-            fieldObj.Set(e, gameObject.GetGrandsChild(0, e.z, e.y, e.x));
-        }
-        Vector3Int cnt = Vector3Int.one * (fieldSize / 2 - 1);
-        Collection.Map(Collection.Enumerate(2, 2, 2), (e) => field.Set(cnt, e, e.Sum() % 2 == 0 ? 1 : 2));
+        _prefabs[Stone.None] = emptyStonePrefab;
+        _prefabs[Stone.Black] = blackStonePrefab;
+        _prefabs[Stone.White] = whiteStonePrefab;
 
-        Display(fieldObj, field, fieldSize);
+        // Set up the field.
+        Array.Fill(_fieldObj, null);
+        Array.Fill(_field, Stone.None);
+
+        foreach (var (position, stone) in InitialStones)
+        {
+            SetStone(position, stone);
+        }
+
+        Display();
     }
 
-    void Update()
+    private void Update()
     {
         // Get the player's input.
-        if (KeyCode.Return.Down())
+        if (Keyboard.current?.enterKey.wasPressedThisFrame == true)
         {
-            int inputNum = int.Parse(PlayerInput.text);
-            (int x, int _y) = Math.DivMod(inputNum, 100);
-            (int y, int z) = Math.DivMod(_y, 10);
-            Put(field, fieldSize, new Vector3Int(x, y, z).Clamp(0, fieldSize - 1), isBlackTurn ? 1 : 2);
-            Display(fieldObj, field, fieldSize);
+            int x, y, z;
+            try
+            {
+                var inputText = inputField.text;
 
-            isBlackTurn = !isBlackTurn;
-            PlayerInput.text = "";
+                var xChar = inputText[0];
+                var yChar = inputText[1];
+                var zChar = inputText[2];
+
+                x = xChar - '0';
+                y = yChar - '0';
+                z = zChar - '0';
+
+                if (x is not (>= 0 and < FieldSize)) throw new Exception("Invalid position.");
+                if (y is not (>= 0 and < FieldSize)) throw new Exception("Invalid position.");
+                if (z is not (>= 0 and < FieldSize)) throw new Exception("Invalid position.");
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e);
+                return;
+            }
+
+            Put(new Vector3Int(x, y, z), _isBlackTurn ? Stone.Black : Stone.White);
+            Display();
+
+            _isBlackTurn = !_isBlackTurn;
+            inputField.text = "";
         }
     }
 
-    // 1:black, 2:white.
-    void Put(int[,,] f, int size, Vector3Int p, int col)
+    private void Put(Vector3Int position, Stone stone)
     {
-        if (col == 0) throw new System.Exception("Invalid color.");
+        if (stone == Stone.None) throw new Exception("Invalid color.");
+        if (GetStone(position) != Stone.None) return;
 
-        if (f.Where(p) != 0) return;
-        else f.Set(p, col);
+        SetStone(position, stone);
 
-        foreach (Vector3Int _e in Collection.Enumerate((-1, 2), (-1, 2), (-1, 2)))
+        for (var x = -1; x <= 1; x++)
+        for (var y = -1; y <= 1; y++)
+        for (var z = -1; z <= 1; z++)
         {
-            Vector3Int e = (p + _e).Clamp(0, size - 1);
-            if (e != p && f.Where(e) == RevCol(col)) Rev(f, size, p, col, e - p);
+            if (x == 0 && y == 0 && z == 0) continue;
+
+            Vector3Int e = position + new Vector3Int(x, y, z);
+            if (e.x is not (>= 0 and < FieldSize)) continue;
+            if (e.y is not (>= 0 and < FieldSize)) continue;
+            if (e.z is not (>= 0 and < FieldSize)) continue;
+
+            if (GetStone(e) == GetOpponentStone(stone))
+            {
+                ReverseLine(position, stone, e - position);
+            }
         }
     }
 
-    void Rev(int[,,] f, int size, Vector3Int p, int col, Vector3Int dir)
+    private void ReverseLine(Vector3Int position, Stone stone, Vector3Int direction)
     {
         List<Vector3Int> revPosMemo = new();
 
-        for (int i = 0; i < size; i++, p += dir)
+        for (int i = 0; i < FieldSize; i++, position += direction)
         {
             // On 0th loop, p is the put position.
             if (i == 0) continue;
 
             // Reached the edge.
-            if (!p.IsIn(0, size - 1)) break;
+            if (position.x is not (>= 0 and < FieldSize)) continue;
+            if (position.y is not (>= 0 and < FieldSize)) continue;
+            if (position.z is not (>= 0 and < FieldSize)) continue;
 
-            if (f.Where(p) == RevCol(col)) revPosMemo.Add(new(p.x, p.y, p.z));
-            else if (f.Where(p) == col) Collection.Map(revPosMemo, (e) => f.Set(e, col));
+            if (GetStone(position) == GetOpponentStone(stone)) revPosMemo.Add(new(position.x, position.y, position.z));
+            else if (GetStone(position) == stone)
+            {
+                foreach (var memo in revPosMemo)
+                    SetStone(memo, stone);
+            }
             else return;
         }
     }
 
-    void Display(GameObject[,,] fieldObj, int[,,] field, int size)
+    private void Display()
     {
-        foreach (Vector3Int e in Collection.Enumerate(size, size, size))
+        for (var x = 0; x < FieldSize; x++)
+        for (var y = 0; y < FieldSize; y++)
+        for (var z = 0; z < FieldSize; z++)
         {
-            Transform parent = fieldObj.Where(e).transform.parent;
-            Destroy(fieldObj.Where(e));
-            fieldObj.Set(e, Instantiate(Prfbs[field.Where(e)], e, Quaternion.identity, parent));
+            var e = new Vector3Int(x, y, z);
+
+            var fieldObject = GetFieldObject(e);
+            if (fieldObject != null)
+            {
+                Destroy(fieldObject);
+            }
+
+            var newObject = Instantiate(_prefabs[GetStone(e)], e, Quaternion.identity, transform);
+            SetFieldObject(e, newObject);
         }
     }
 
-    int RevCol(int col)
+    private GameObject GetFieldObject(Vector3Int position)
+        => _fieldObj[GetIndex(position)];
+
+    private void SetFieldObject(Vector3Int position, GameObject obj)
+        => _fieldObj[GetIndex(position)] = obj;
+
+    private Stone GetStone(Vector3Int position)
+        => _field[GetIndex(position)];
+
+    private void SetStone(Vector3Int position, Stone stone)
+        => _field[GetIndex(position)] = stone;
+
+    private static int GetIndex(Vector3Int position)
+        => position.x * FieldSize * FieldSize + position.y * FieldSize + position.z;
+
+    private static Stone GetOpponentStone(Stone stone) => stone switch
     {
-        if (col == 1) return 2;
-        else if (col == 2) return 1;
-        else throw new System.Exception("Invalid color.");
-    }
+        Stone.Black => Stone.White,
+        Stone.White => Stone.Black,
+        _           => throw new Exception("Invalid stone color.")
+    };
 }
